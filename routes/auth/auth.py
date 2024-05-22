0affcbf642ef5a41d4c4d25ed86e103676b071a6
TehGaa <syosuasaputra1010@gmail.com>
2024-05-23 00:14:56 +0700

    fix raise error

--- routes/auth/auth.py
+++ routes/auth/auth.py
@@ -27,33 +27,33 @@ async def login(user: UserRequest):
     result = requests.post(api_url + "/login", json=dataclasses.asdict(user))   
     if result.status_code == 201:
         return result.json()
-    return HTTPException(status_code=result.status_code, detail=result.json().get("message"))
+    raise HTTPException(status_code=result.status_code, detail=result.json().get("message"))
 
 @router.post("/register")
 async def register(user: UserRequest):
     result = requests.post(api_url + "/register", json=dataclasses.asdict(user))
     if (result.status_code == 201):
         return result.json()
-    return HTTPException(status_code=result.status_code, detail=result.json().get("message"))
+    raise HTTPException(status_code=result.status_code, detail=result.json().get("message"))
 
 @router.get("/refresh")
 async def refresh(request: Request):
     if "Authorization" not in request.headers:
-        return HTTPException(status_code=401, detail="Unauthorized")
+        raise HTTPException(status_code=401, detail="Unauthorized")
     token = request.headers["Authorization"]
     result = requests.get(api_url + "/refresh", headers={"Authorization": token})
     if (result.status_code == 200):
         return result.json()
-    return HTTPException(status_code=result.status_code, detail=result.json().get("message"))
+    raise HTTPException(status_code=result.status_code, detail=result.json().get("message"))
 
 @router.get("/get-data-from-token")
 async def get_data_from_token(request: Request):
     if "Authorization" not in request.headers:
-        return HTTPException(status_code=401, detail="Unauthorized")
+        raise HTTPException(status_code=401, detail="Unauthorized")
     token = request.headers["Authorization"]
     result = requests.get(api_url + "/get-data-from-token", headers={"Authorization": token})
     if (result.status_code == 200):
         return result.json()
-    return HTTPException(status_code=result.status_code, detail=result.json().get("message"))
+    raise HTTPException(status_code=result.status_code, detail=result.json().get("message"))
 
 
