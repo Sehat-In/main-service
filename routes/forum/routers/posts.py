751df37f6532fe37c900fe3b8f11e0c647dad087
TehGaa <syosuasaputra1010@gmail.com>
2024-05-25 19:59:09 +0700

    fix: fix check notification

--- routes/forum/routers/posts.py
+++ routes/forum/routers/posts.py
@@ -13,14 +13,14 @@ api_url = os.getenv("FORUM_API_URL")
 async def check_notification(username: str):
     result = requests.get(api_url + f"/api/v1/posts/check-notification/{username}")
     if result.status_code == 200:
-        return result.json()
+        return result.text
     raise HTTPException(status_code=result.status_code, detail=result.json().get("message"))
 
 @router.get("/get-notifications/{username}")
 async def get_notifications(username: str):
     result = requests.get(api_url + f"/api/v1/posts/get-notifications/{username}")
     if result.status_code == 200:
-        return result.json()
+        return result
     raise HTTPException(status_code=result.status_code, detail=result.json().get("message"))
 
 @router.get("/remove-notifications/{username}")
