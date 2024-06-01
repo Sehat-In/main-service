375e6546014078986a890991756f2d2b0dc62eac
luluorv <theaaryanto2004@gmail.com>
2024-06-01 14:09:06 +0700

    added comments and likes router

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi import APIRouter
 from routes.auth import auth
-from routes.forum.routers import posts
+from routes.forum.routers import posts, comments, likes
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -28,3 +28,5 @@ async def hello():
 app.include_router(router1, prefix="/api/v1")
 app.include_router(auth.router, prefix="/api/v1/auth")  
 app.include_router(posts.router, prefix="/api/v1/posts")
+app.include_router(comments.router, prefix="/api/v1/comments")
+app.include_router(likes.router, prefix="/api/v1/likes")
